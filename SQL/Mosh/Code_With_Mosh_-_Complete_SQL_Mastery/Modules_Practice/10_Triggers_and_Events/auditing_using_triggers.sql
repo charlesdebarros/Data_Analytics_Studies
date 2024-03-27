489a62c7995fbeea9078e30dda793b4c9d5a5735
Charles De Barros <charlesdebarros@outlook.com>
2024-03-27 16:17:44 +0000

    Adding Auditing using Triggers files

---
+++ SQL/Mosh/Code_With_Mosh_-_Complete_SQL_Mastery/Modules_Practice/10_Triggers_and_Events/auditing_using_triggers.sql
@@ -0,0 +1,23 @@
+-- Auditing using TRIGGERS
+--
+-- Script to create payments_audit table
+--
+-- USE sql_invoicing;
+-- 
+-- CREATE TABLE payments_audit
+-- (
+-- 		client_id	INT 			NOT NULL,
+--    	date		DATE			NOT NULL,
+--    	amount		DECIMAL(9, 2) 	NOT NULL,
+--    	action_type	VARCHAR(50)		NOT NULL,
+--    	action_date	DATETIME		NOT NULL
+-- )
+
+-- See audit lines added to the payments table.
+
+-- USE sql_invoicing;
+
+-- INSERT INTO payments
+-- VALUES (DEFAULT, 5, 3, '2019-01-01', 10, 1);
+
+-- DELETE FROM payments WHERE payment_id = 13;
--- SQL/Mosh/Code_With_Mosh_-_Complete_SQL_Mastery/Modules_Practice/Triggers/payment_after_delete_trigger.sql
+++ SQL/Mosh/Code_With_Mosh_-_Complete_SQL_Mastery/Modules_Practice/Triggers/payment_after_delete_trigger.sql
@@ -10,6 +10,9 @@ BEGIN
 	UPDATE invoices
     SET payment_total = payment_total - OLD.amount
     WHERE invoice_id = OLD.invoice_id;
+    
+    INSERT INTO payments_audit
+	VALUES (OLD.client_id, OLD.date, OLD.amount, 'Delete', NOW());
 END $$
 
 DELIMITER ;
